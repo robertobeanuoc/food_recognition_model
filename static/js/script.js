0a9a8fc07e66650adaf3ce50062758b106082d00
robertobeanuoc <rbean@uoc.edu>
2024-06-30 16:38:06 +0200

    fix: size

--- static/js/script.js
+++ static/js/script.js
@@ -37,3 +37,34 @@ snap.addEventListener('click', () => {
         });
     }, 'image/jpeg');
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    adjustCanvasSize();
+
+    window.addEventListener('resize', adjustCanvasSize);
+});
+
+function adjustCanvasSize() {
+    var canvas = document.getElementById('video');
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var aspectRatio = 16 / 9; // Example aspect ratio
+
+    console.log('Adjusting size..');
+
+    // Adjust width and height according to the aspect ratio
+    if (width / height > aspectRatio) {
+        // If window is wider than our desired aspect ratio
+        canvas.width = height * aspectRatio;
+        canvas.height = height;
+    } else {
+        // If window is taller than our desired aspect ratio
+        canvas.width = width;
+        canvas.height = width / aspectRatio;
+    }
+
+    // Optional: Adjust canvas style to center it, if desired
+    canvas.style.marginLeft = (window.innerWidth - canvas.width) / 2 + 'px';
+    canvas.style.marginTop = (window.innerHeight - canvas.height) / 2 + 'px';
+}
