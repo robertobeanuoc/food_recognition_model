0fc4acd6294693dd10b8c09b3b5efe6f3b7c171d
robertobeanuoc <rbean@uoc.edu>
2024-07-01 22:09:00 +0200

    fix: aspect ratio

--- static/js/script.js
+++ static/js/script.js
@@ -4,9 +4,12 @@ const snap = document.getElementById('snap');
 const fileInput = document.getElementById('file');
 const uploadForm = document.getElementById('upload-form');
 
+let localStream;
+
 navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"} , width: {min:720}, height: { min: 1280} })
     .then(stream => {
-        video.srcObject = stream;
+        localStream = stream;
+        video.srcObject = localStream;
     })
     .catch(err => {
         console.error("Error accessing camera: ", err);
@@ -49,9 +52,18 @@ function adjustCanvasSize() {
     var canvas = document.getElementById('video');
     var width = window.innerWidth;
     var height = window.innerHeight;
-    var aspectRatio = 16 / 9; // Example aspect ratio
+    var aspectRatio = 9 / 16; // Example aspect ratio
+    if (width > height) {
+        aspectRatio = 16 / 9;
+    }
+    var aspectRatio = 9 / 16; // Example aspect ratio
 
     console.log('Adjusting size..');
+
+    // Adjust stream size to 80% of window size
+    // const width = window.innerWidth * 0.8;
+    // const height = window.innerHeight * 0.8;
+    
 
     // Adjust width and height according to the aspect ratio
     if (width / height > aspectRatio) {
@@ -67,4 +79,7 @@ function adjustCanvasSize() {
     // Optional: Adjust canvas style to center it, if desired
     canvas.style.marginLeft = (window.innerWidth - canvas.width) / 2 + 'px';
     canvas.style.marginTop = (window.innerHeight - canvas.height) / 2 + 'px';
+    video.srcObject.width = canvas.width * 0.95;
+    video.srcObject.height = canvas.height * 0.95;
+
 }
