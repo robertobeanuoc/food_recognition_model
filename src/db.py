4b8cad38c662fc3531a3a154131410896b1eea0b
robertobeanuoc <rbean@uoc.edu>
2024-07-07 16:59:59 +0200

    fea: add food clasification

--- src/db.py
+++ src/db.py
@@ -6,12 +6,7 @@ from utils import app_logger
 
 def insert_food_type(file_uid:str, food_type:str, glycemic_index:int, weight_grams:int, created_at:datetime.datetime=datetime.datetime.now()):
 
-    cnx: mysql.connector.MySQLConnection = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
+    cnx:mysql.connector.MySQLConnection = _connect_to_db()
     
     app_logger.info("Connected to the database")
 
@@ -35,3 +30,50 @@ def insert_food_type(file_uid:str, food_type:str, glycemic_index:int, weight_gra
     cursor.close()
     cnx.close()
     app_logger.info("Connection closed")
+
+def _connect_to_db()-> mysql.connector.MySQLConnection:
+    cnx: mysql.connector.MySQLConnection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+    return cnx
+
+
+def get_food_types()-> list[dict]:
+    cnx: mysql.connector.MySQLConnection = _connect_to_db()
+    app_logger.info("Connected to the database")
+
+    # Create a cursor object to execute SQL queries
+    cursor = cnx.cursor()
+
+    # Define the SQL query to retrieve all records from the food_table
+    query:str = "SELECT food_type, food_type_es, glycemic_index FROM glycemic_index"
+    app_logger.info(f"Query: {query}")
+
+    # Execute the query
+    cursor.execute(query)
+    app_logger.info("Query executed successfully")
+
+    # Fetch all the records
+    records = cursor.fetchall()
+    records_json = []
+    for record in records:
+        record_dict = {
+            'food_type': record[0],
+            'food_type_es': record[1],
+            'glycemic_index': record[2],
+        }
+        records_json.append(record_dict)
+    app_logger.info("Records fetched")
+
+    # Close the cursor and the connection
+    cursor.close()
+    cnx.close()
+    app_logger.info("Connection closed")
+
+    # Return the records as JSON
+    app_logger.info("Records fetched")
+    return records_json
+    
---
+++ src/test/test_similar.py
@@ -0,0 +1,9 @@
+from db import get_food_types
+
+
+def test_get_food_types():
+    get_food_types()
+
+
+test_get_food_types()
+
