4ee5392550b33064ac0f5242d5b31b3efa27a67f
robertobeanuoc <rbean@uoc.edu>
2024-07-14 12:09:44 +0200

    fix: R update value

--- src/analysis/difference_similar.R
+++ src/analysis/difference_similar.R
@@ -1,7 +1,10 @@
 library(psych)
 library(ggplot2)
 
-food_comparation <- read.csv("../../data/food_compare.csv", sep = ",", stringsAsFactors = F)
+food_comparation_raw <- read.csv("../../data/food_compare.csv", sep = ",", stringsAsFactors = F)
+
+food_comparation <- food_comparation_raw[!food_comparation_raw$glycemic_index_difference %in% "None",]
+food_comparation$glycemic_index_difference = as.integer(food_comparation$glycemic_index_difference)
 
 
 describe(food_comparation$glycemic_index_difference)
