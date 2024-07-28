95aff8e110290918e789ac0fe88dd82c12c7f4d3
robertobeanuoc <rbean@uoc.edu>
2024-07-28 18:13:48 +0200

    feat: rename method

--- src/food_recognition/static/js/food_register.js
+++ src/food_recognition/static/js/food_register.js
@@ -4,7 +4,6 @@ function updateVerified(uuid, verified) {
         int_verified = 1;
     }
     const url = `/update_verified/${uuid}/${int_verified}`;
-    
     // Remove the extra closing curly brace
     fetch(url, {
         method: 'GET',
@@ -14,5 +13,26 @@ function updateVerified(uuid, verified) {
           // Additional headers can be added here if needed
         },
     })
-    console.log(message);
+}
+
+function updateFoodRegister(index) 
+{
+    var foodType = document.getElementById('food_type_' + index).value;
+    var glycemicIndex = document.getElementById('glycemic_index_' + index).value;
+    var weightGrams = document.getElementById('weight_grams_' + index).value;
+    var uuid = document.getElementById('uuid_' + index).value;
+    var verified = document.getElementById('verified_' + index).checked ? 1 : 0;
+
+    console.log('foodType:', foodType, "glycemicIndex:", glycemicIndex, "uuid:", uuid);
+
+    const url = '/update_food_register/'+ uuid + "/" +  foodType + '/' + glycemicIndex + "/" + weightGrams + "/" + verified;
+    fetch(url, {
+        method: 'GET',
+        credentials: 'include', // Include credentials in case of cookies/session authentication
+        headers: {
+          'Content-Type': 'application/json',
+          // Additional headers can be added here if needed
+        },
+    })
+
 }
